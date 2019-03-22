ae39be7be548f9086d72d6e5435cb0a471e04282
andrew <36473866+andrew-MET@users.noreply.github.com>
2019-03-22 09:24:11 +0000

    Allow lagging to be done forwards in time.

--- R/lag_forecast.R
+++ R/lag_forecast.R
@@ -8,15 +8,21 @@
 #' @param fcst_model The name of the forecast model in the harp_fcst object to
 #'   be lagged. Must be quoted.
 #' @param parent_cycles A numeric vector of forecast cycles that form the
-#'   parents for the lagging. Members between parent cycles will be added to the
-#'   parent cycle that is the closest that comes after the child cycle.
+#'   parents for the lagging. Members between parent cycles are the child
+#'   cycles.
+#' @param dierction The direction to do the lagging in. 1 (the default) lags
+#'   backwards in time from the parent cycles and -1 lags forwards in time.
 #'
 #' @return A harp_fcst object with \code{fcst_model} now containing the lagged
 #'   forecast.
 #' @export
 #'
 #' @examples
-lag_forecast <- function(.fcst, fcst_model, parent_cycles) {
+lag_forecast <- function(.fcst, fcst_model, parent_cycles, direction = 1) {
+
+  if (!direction %in% c(1, -1)) {
+    stop("direction must be 1 to lag backwards in time, or -1 to lag forwards in time", call. = FALSE)
+  }
 
   fcst_df <- .fcst[[fcst_model]]
 
@@ -30,49 +36,56 @@ lag_forecast <- function(.fcst, fcst_model, parent_cycles) {
     dplyr::arrange(.data$fcst_cycle) %>%
     dplyr::mutate(
       parent_cycle = purrr::map_chr(
-        .data$fcst_cycle, ~ formatC(find_parent(as.numeric(.x), parent_cycles), width = 2, flag = "0")
+        .data$fcst_cycle, ~ formatC(find_parent(as.numeric(.x), parent_cycles, direction), width = 2, flag = "0")
       )
     )
 
-  .fcst[[fcst_model]] <- purrr::map_dfr(split(fcst_df, fcst_df$parent_cycle), lag_cycle)
+  .fcst[[fcst_model]] <- purrr::map_dfr(split(fcst_df, fcst_df$parent_cycle), lag_cycle, direction)
 
   purrr::map(.fcst, tidyr::drop_na) %>%
     new_harp_fcst()
 
 }
 
-find_parent <- function(val, vec) {
-  diffs <- vec - val
-  if (all(diffs < 0)) {
-    diffs <- diffs - diffs[1]
+find_parent <- function(val, vec, direction) {
+  if (direction == 1) {
+    diffs <- vec - val
+    if (all(diffs < 0)) {
+      diffs <- diffs - diffs[1]
+    }
+  } else {
+    diffs <- val - vec
+    if (all(diffs < 0)) {
+      diffs <- diffs - diffs[length(diffs)]
+    }
   }
-  vec[which(diffs == min(diffs[diffs >= 0]))]
+    vec[which(diffs == min(diffs[diffs >= 0]))]
 }
 
-lag_cycle <- function(df) {
+lag_cycle <- function(df, direction) {
   parent_cycle   <- unique(df$parent_cycle)
-  lags           <- df$fcst_cycle[df$fcst_cycle != parent_cycle]
-  num_lags       <- length(lags)
+  child_cycles   <- df$fcst_cycle[df$fcst_cycle != parent_cycle]
+  num_children   <- length(child_cycles)
   lagged_df      <- dplyr::filter(df, .data$fcst_cycle == parent_cycle) %>%
     tidyr::unnest() %>%
     dplyr::select_if(~ !all(is.na(.))) %>%
     dplyr::select(-.data$parent_cycle)
 
-  if (num_lags > 0) {
+  if (num_children > 0) {
 
-    for (i in 1:num_lags) {
+    for (i in 1:num_children) {
 
-      lag_hours <- as.numeric(parent_cycle) - as.numeric(lags[i])
+      lag_hours <- (as.numeric(parent_cycle) - as.numeric(child_cycles[i])) * direction
       lag_hours[lag_hours < 0] <- lag_hours[lag_hours < 0] + 24
 
       lagged_df <- dplyr::inner_join(
         lagged_df,
-        dplyr::filter(df, .data$fcst_cycle == lags[i]) %>%
+        dplyr::filter(df, .data$fcst_cycle == child_cycles[i]) %>%
           tidyr::unnest() %>%
           dplyr::select_if(~ !all(is.na(.))) %>%
           dplyr::mutate(
-            leadtime = .data$leadtime - lag_hours,
-            fcdate   = .data$fcdate   + lag_hours * 3600
+            leadtime = .data$leadtime - lag_hours * direction,
+            fcdate   = .data$fcdate   + lag_hours * 3600 * direction
           ) %>%
           dplyr::select(-.data$fcst_cycle, -.data$parent_cycle),
         by = intersect(c("SID", "fcdate", "leadtime", "validdate", "parameter", "units"), colnames(lagged_df)),
