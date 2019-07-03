65caa546ac10352c0511c951207baa606f0076bd
andrew <36473866+andrew-MET@users.noreply.github.com>
2019-07-03 12:31:15 +0000

    Enable units scaling and joining.

---
+++ R/data_units_and_scaling.R
@@ -0,0 +1,171 @@
+#' Set the units for meteorological point data.
+#'
+#' Note that the contents of the data are not changed in any way. Rather, if a
+#' column called 'units' exists, its contents are modified to be that passed in
+#' the units_name argument - if the 'units' column does not exist, it is added.
+#'
+#' @param point_data A harp_fcst object, or a data frame.
+#' @param units_name The name of the units of the data, e.g. "m/s", "kg/m\^2" etc.
+#'
+#' @return A An object of the same type as \code{point_data}, but with the units
+#'   column modified to contain \code{units_name}, or the units column added.
+#' @export
+#'
+#' @examples
+set_units <- function(point_data, units_name) {
+  UseMethod("set_units")
+}
+
+#' @export
+set_units.default <- function(point_data, units_name) {
+
+  # Check for data columns so that units is added between the metadata and the data
+
+  # Ensemble forecast data
+  data_cols <- grep("[[:graph:]]+mbr+[[:digit:]]+$", colnames(point_data), perl = TRUE, value = TRUE)
+
+  # Deterministic forecast data
+  if (length(data_cols) < 1) {
+    data_cols <- grep("[[:graph:]]+det$", colnames(point_data), perl = TRUE, value = TRUE)
+  }
+
+  # Other (probably observations) data
+  if (length(data_cols) < 1) {
+    data_cols <- setdiff(colnames(point_data), c("SID" , "validdate", "lat", "lon", "units"))
+  }
+
+  # Mutate units column and return data frame with metadata columns followed by data columns
+  metadata_cols <- setdiff(colnames(point_data), data_cols)
+  if (!is.element("units", metadata_cols)) metadata_cols <- c(metadata_cols, "units")
+
+  point_data <- dplyr::mutate(point_data, units = units_name)
+
+  point_data[c(metadata_cols, data_cols)]
+
+}
+
+#' @export
+set_units.harp_fcst <- function(point_data, units_name) {
+  new_harp_fcst(purrr::map(point_data, set_units, units_name))
+}
+
+
+#' Scale forecast data
+#'
+#' If you wish to scale the forecast values, for example when temperature data
+#' are in Kelvin and you want them in degrees C, this function can be used to
+#' scale the data.
+#'
+#' @param .fcst A \code{harp_fcst} object as read in by read_point_forecast from
+#'   harpIO.
+#' @param scale_factor The scaling factor.
+#' @param new_units The name of the new units - if set to NULL, the name is not
+#'   changed.
+#' @param multiplicative If the scaling is to be done multiplicatively, i.e. the
+#'   new forecast value is to be the old value * scale_factor, set
+#'   multiplicative to TRUE. The defualt (multiplicative = FALSE) is to do the
+#'   scaling additively, i.e. the new forecast value is the old value +
+#'   scale_factor.
+#'
+#' @return A \code{harp_fcst} object with forecast values scaled by
+#'   \code{scale_factor}.
+#' @export
+#'
+#' @examples
+scale_point_forecast <- function(.fcst, scale_factor, new_units = NULL, multiplicative = FALSE) {
+  UseMethod("scale_point_forecast")
+}
+
+#' @export
+scale_point_forecast.default <- function(.fcst, scale_factor, new_units = NULL, multiplicative = FALSE) {
+
+  if (multiplicative) {
+    scale_function <- function(x, sf) {
+      x * sf
+    }
+  } else {
+    scale_function <- function(x, sf) {
+      x + sf
+    }
+  }
+
+  # Ensemble forecast data
+  data_cols <- grep("[[:graph:]]+mbr+[[:digit:]]+$", colnames(.fcst), perl = TRUE, value = TRUE)
+
+  # Deterministic forecast data
+  if (length(data_cols) < 1) {
+    data_cols <- grep("[[:graph:]]+det$", colnames(.fcst), perl = TRUE, value = TRUE)
+  }
+
+  # Error
+  if (length(data_cols) < 1) {
+    stop("No forecast data columns found. Are you sure this is a harp_fcst object?", call. = FALSE)
+  }
+
+  .fcst <- dplyr::mutate_at(.fcst, data_cols, scale_function, scale_factor)
+
+  # Add or modify units if new_units is passed
+  if (!is.null(new_units)) {
+    .fcst <- set_units(.fcst, new_units)
+  }
+
+  .fcst
+}
+
+#' @export
+scale_point_forecast.harp_fcst <- function(.fcst, scale_factor, new_units = NULL, multiplicative = FALSE) {
+  new_harp_fcst(purrr::map(.fcst, scale_point_forecast, scale_factor, new_units, multiplicative))
+}
+
+
+#' Scale observations data
+#'
+#' If you wish to scale the observations values, for example when temperature
+#' data are in Kelvin and you want them in degrees C, this function can be used
+#' to scale the data.
+#'
+#' @param .obs A data frame of point observations as read in by read in by
+#'   read_point_obs from harpIO.
+#' @param parameter The column name of the data to be scaled. Must be unquoted.
+#' @param scale_factor The scaling factor.
+#' @param new_units The name of the new units - if set to NULL, the name is not
+#'   changed.
+#' @param multiplicative If the scaling is to be done multiplicatively, i.e. the
+#'   new forecast value is to be the old value * scale_factor, set
+#'   multiplicative to TRUE. The defualt (multiplicative = FALSE) is to do the
+#'   scaling additively, i.e. the new forecast value is the old value +
+#'   scale_factor.
+#'
+#' @return The observations data frame with the parameter column scaled by
+#'   \code{scale_factor}.
+#' @export
+#'
+#' @examples
+scale_point_obs <- function(.obs, parameter, scale_factor, new_units = NULL, multiplicative = FALSE) {
+
+  parameter      <- rlang::enquo(parameter)
+  parameter_name <- rlang::quo_name(parameter)
+
+  if (!is.element(parameter_name, colnames(.obs))) {
+    stop("Parameter \"", parameter_name, "\" not found in .obs", call. = FALSE)
+  }
+
+  if (multiplicative) {
+    scale_function <- function(x, sf) {
+      x * sf
+    }
+  } else {
+    scale_function <- function(x, sf) {
+      x + sf
+    }
+  }
+
+  .obs <- dplyr::mutate(.obs, !!parameter_name := scale_function(!!parameter, scale_factor))
+
+  if (!is.null(new_units)) {
+    .obs <- set_units(.obs, new_units)
+  }
+
+  .obs
+
+}
--- R/join_to_fcst.R
+++ R/join_to_fcst.R
@@ -3,15 +3,26 @@
 #' Works in a similar way to the \link[dplyr]{join} family of functions, excpet
 #' that the join type is specified as an argument.
 #'
+#' This function would typically be used to add a column of observations to the
+#' forecast data. If units columns are found in the data, the function will
+#' check if the units are the same for both .fcst and .join. If they are not, an
+#' error will be thrown and the join will not be done. This behaviour can be
+#' overridden with the force_join argument.
+#'
+#'
 #' @param .fcst A \code{harp_fcst} object.
 #' @param .join A data frame to join to each table in the \code{harp_fcst}
 #'   object.
 #' @param join_type How to join the data frame. Acceptable values are: "inner",
 #'   "left", "right", "full", "semi", "anti". See \code{\link[dplyr]{join}} for
 #'   more details.
-#' @param by Which columns to join by.
+#' @param by Which columns to join by - if set to NULL a natural join will be
+#'   done, using all variables with common names across .fcst and .join.
+#' @param force_join Set to TRUE to force the join to happen even if the units
+#'   in .fcst and .join are not compatible.
+#' @param ... Other arguments for \link[dplyr]{join}.
 #'
-#' @return The input forecast data fram with an "obs" column added.
+#' @return The input forecast data frame with column(s) added from \code{.join}.
 #' @export
 #'
 #' @examples
@@ -19,7 +30,9 @@
 join_to_fcst <- function(
   .fcst,
   .join,
-  join_type = c("inner", "left", "right", "full", "semi", "anti"),
+  join_type  = c("inner", "left", "right", "full", "semi", "anti"),
+  by         = NULL,
+  force_join = FALSE,
   ...
 ) {
 
@@ -31,23 +44,75 @@ join_to_fcst <- function(
 join_to_fcst.default <- function(
   .fcst,
   .join,
-  join_type = c("inner", "left", "right", "full", "semi", "anti"),
+  join_type  = c("inner", "left", "right", "full", "semi", "anti"),
+  by         = NULL,
+  force_join = FALSE,
   ...
 ) {
 
-  valid_joins <- c("inner", "left", "right", "full", "semi", "anti")
-  if (length(intersect(join_type, valid_joins)) < 1) {
-    stop(
-      paste(
-        "Invalid join_type: ", join_type[1], ".\n",
-        "Must be one of: 'inner', 'left', 'right', 'full', 'semi', 'anti'"
-      )
-    )
+  join_type <- match.arg(join_type)
+
+  join_func <- get(paste0(join_type, "_join"), envir = asNamespace("dplyr"))
+
+  # Check for units columns
+  has_fcst_units <- is.element("units", colnames(.fcst))
+  has_join_units <- is.element("units", colnames(.join))
+
+  if (has_fcst_units & has_join_units) {
+
+    do_join <- TRUE
+    fcst_units <- unique(.fcst$units)
+    join_units <- unique(.join$units)
+
+    if (length(fcst_units) != 1) {
+      warning(".fcst has more than one units name: ", fcst_units, call. = FALSE, immediate. = TRUE)
+      do_join <- FALSE
+    } else if (length(unique(.fcst$units)) != 1) {
+      warning(".join has more than one units name: ", join_units, call. = FALSE, immediate. = TRUE)
+      do_join <- FALSE
+    } else {
+      if (fcst_units != join_units) {
+        warning(".fcst has units: ", fcst_units, " and .join has units: ", join_units, call. = FALSE, immediate. = TRUE)
+        do_join <- FALSE
+      }
+    }
+
+  } else if (has_fcst_units & !has_join_units) {
+
+    warning(".join does not have a units column. ", call. = FALSE, immediate. = TRUE)
+    do_join <- FALSE
+
+  } else if (!has_fcst_units & has_join_units) {
+
+    warning(".fcst does not have a units column. ", call. = FALSE, immediate. = TRUE)
+    do_join <- FALSE
+
+  } else {
+
+    warning("Neither .fcst nor .join have a units column.", call. = FALSE, immediate. = TRUE)
+    do_join <- TRUE
+
   }
 
-  join_func <- get(paste0(join_type[1], "_join"), envir = asNamespace("dplyr"))
+  if (!do_join) {
+    if (force_join) {
+      message("Forcing join without units taken into account.")
+      by <- intersect(colnames(.fcst), colnames(.join))
+      by <- by[by != "units"]
+    } else {
+      stop(
+        "Join will not be done due to units incompatibility. You can force the join by setting force_join = TRUE\n",
+        "OR, units imcompatibility can be fixed with the set_units, scale_point_forecast, or scale_point_obs functions.",
+        call. = FALSE
+      )
+    }
+  }
 
-  join_func(.fcst, .join, ...)
+  if (!is.null(by)) {
+    message("Joining, by = c(\"", paste(by, collapse = "\", \""), "\")")
+  }
+
+  join_func(.fcst, .join, by = by, ...)
 
 }
 
@@ -55,10 +120,12 @@ join_to_fcst.default <- function(
 join_to_fcst.harp_fcst <- function(
   .fcst,
   .join,
-  join_type = c("inner", "left", "right", "full", "semi", "anti"),
+  join_type  = c("inner", "left", "right", "full", "semi", "anti"),
+  by         = NULL,
+  force_join = FALSE,
   ...
 ) {
 
-  new_harp_fcst(purrr::map(.fcst, join_to_fcst, .join, join_type, ...))
+  try(new_harp_fcst(purrr::map(.fcst, join_to_fcst, .join, join_type, by, force_join, ...)))
 
 }
