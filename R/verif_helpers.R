79de08de9649feefbdb13a3c6fa7b138039c8631
Andrew Singleton <andrewts@met.no>
2023-10-31 09:20:11 +0100

    Fix fill_group_na for complex group lists

--- R/verif_helpers.R
+++ R/verif_helpers.R
@@ -20,34 +20,55 @@ fill_group_na <- function(df, groupings) {
 
   all_groups     <- unique(unlist(groupings))
   group_count    <- sapply(all_groups, function(x) sum(unlist(groupings) == x))
-  groups_to_fill <- names(group_count)[group_count < length(all_groups)]
-  groups_to_fill <- groups_to_fill[groups_to_fill != "threshold"]
+#  groups_to_fill <- names(group_count)[group_count < length(all_groups)]
+#  groups_to_fill <- groups_to_fill[groups_to_fill != "threshold"]
+  groups_to_fill <- all_groups[all_groups != "threshold"]
+  if (length(groups_to_fill) < 1) {
+    return(df)
+  }
 
-  fill_fun <- function(group_col, df) {
-    if (!any(is.na(df[[group_col]]))) return(df)
-    all_values <- sort(unique(stats::na.omit(df[[group_col]])))
-    df[[group_col]] <- as.character(df[[group_col]])
+  # fill_fun <- function(group_col, df) {
+  #   if (!any(is.na(df[[group_col]]))) return(df)
+  #   all_values <- sort(unique(stats::na.omit(df[[group_col]])))
+  #   df[[group_col]] <- as.character(df[[group_col]])
+  #   if (length(all_values) < 5) {
+  #     fill_value <- paste0(" ", paste(all_values, collapse = "; "))
+  #   } else {
+  #     fill_value <- "All"
+  #   }
+  #   group_sym <- rlang::sym(group_col)
+  #   dplyr::mutate(
+  #     df,
+  #     !! group_sym := dplyr::case_when(
+  #       is.na(!! group_sym) ~ fill_value,
+  #       TRUE                ~ !! group_sym
+  #     )
+  #   )
+  # }
+
+  fill_fun <- function(x) {
+    if (!any(is.na(x))) {
+      return(x)
+    }
+    all_values <- sort(unique(stats::na.omit(x)))
+    x <- as.character(x)
     if (length(all_values) < 5) {
-      fill_value <- paste0(" ", paste(all_values, collapse = "; "))
+      fill_value = paste(all_values, collapse = "; ")
     } else {
       fill_value <- "All"
     }
-    group_sym <- rlang::sym(group_col)
-    dplyr::mutate(
-      df,
-      !! group_sym := dplyr::case_when(
-        is.na(!! group_sym) ~ fill_value,
-        TRUE                ~ !! group_sym
-      )
-    )
+    x[is.na(x)] <- fill_value
+    x
   }
 
-  if (length(groups_to_fill) > 0) {
-    for(fill_group in groups_to_fill) {
-      df <- fill_fun(fill_group, df)
-    }
-  }
-  df
+  dplyr::mutate(df, dplyr::across(dplyr::any_of(groups_to_fill), fill_fun))
+
+  # if (length(groups_to_fill) > 0) {
+  #   for(fill_group in groups_to_fill) {
+  #     df <- fill_fun(fill_group, df)
+  #   }
+  # }
+  # df
 }
 
 bias <- function(fcst, obs, circle) {
