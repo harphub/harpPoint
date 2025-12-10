cca691def48ca06ac03666388381e0b5dfe26be9
j-fannon <91901684+j-fannon@users.noreply.github.com>
2025-12-10 07:19:50 +0000

    Pass named args to det/ens_verify

--- R/det_verify.R
+++ R/det_verify.R
@@ -245,12 +245,25 @@ det_verify.harp_ens_point_df <- function(
               sub_model = sub_model_name
             )
           ),
-          {{parameter}}, thresholds, clean_thresh,
-          comparator, include_low, include_high,
-          groupings, circle, summary, hexbin, num_bins,
-          dttm_pluck_freq, dttm_pluck_offset,
-          show_progress, new_det_score, new_det_cont_score, new_det_score_opts,
-          fcst_model, ...
+          {{parameter}},
+          thresholds = thresholds,
+          clean_thresh = clean_thresh,
+          comparator = comparator,
+          include_low = include_low,
+          include_high = include_high,
+          groupings = groupings,
+          circle = circle,
+          summary = summary,
+          hexbin = hexbin,
+          num_bins = num_bins,
+          dttm_pluck_freq = dttm_pluck_freq,
+          dttm_pluck_offset = dttm_pluck_offset,
+          show_progress = show_progress,
+          new_det_score = new_det_score,
+          new_det_cont_score = new_det_cont_score,
+          new_det_score_opts = new_det_score_opts,
+          fcst_model = fcst_model,
+          ...
         )
       }
     )
@@ -453,11 +466,26 @@ det_verify.harp_list <- function(
     purrr::imap(
       .fcst,
       ~det_verify(
-        .x, {{parameter}}, thresholds, clean_thresh, comparator,
-        include_low, include_high, groupings, circle, summary, hexbin, num_bins,
-        dttm_pluck_freq, dttm_pluck_offset,
-        show_progress, new_det_score, new_det_cont_score, new_det_score_opts,
-        fcst_model = .y, ...
+        .x,
+        {{parameter}},
+        thresholds = thresholds,
+        clean_thresh = clean_thresh,
+        comparator = comparator,
+        include_low = include_low,
+        include_high = include_high,
+        groupings = groupings,
+        circle = circle,
+        summary = summary,
+        hexbin = hexbin,
+        num_bins = num_bins,
+        dttm_pluck_freq = dttm_pluck_freq,
+        dttm_pluck_offset = dttm_pluck_offset,
+        show_progress = show_progress,
+        new_det_score = new_det_score,
+        new_det_cont_score = new_det_cont_score,
+        new_det_score_opts = new_det_score_opts,
+        fcst_model = .y,
+        ...
       )
     )
   )
