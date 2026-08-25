473719ecc3485ada79802b107e3ae6f5b4ad261b
j-fannon <91901684+j-fannon@users.noreply.github.com>
2026-08-25 11:09:30 +0000

    Don't output range, only thresholds that actually exist

--- R/det_verify.R
+++ R/det_verify.R
@@ -564,14 +564,13 @@ clean_thresholds <- function(all_data, thresholds, comparator) {
   }
 
   if (comparator == "between") {
-    first_last <- range(which(
+    return(thresholds[
       vapply(
         thresholds,
-        function(x) (min(data_range) <= max(x) & max(data_range) >= min(x)),
+        function(x) min(data_range) <= max(x) && max(data_range) >= min(x),
         logical(1)
       )
-    ))
-    return(thresholds[do.call(seq, as.list(first_last))])
+    ])
   }
 
   # Not sure how to handle outside so just return thresholds
